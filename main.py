52647e0d82e4d96ad5e0e1f60ad62c91c6395fdd
MaxAnii <ansarbaba2000@gmail.com>
2024-01-02 22:41:15 +0530

    config the tweepy auth.

---
+++ main.py
@@ -0,0 +1,14 @@
+import tweepy
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+api_key = os.getenv("API_KEY")
+api_key_secret = os.getenv("API_KEY_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+api.update_status("testing the bot")
