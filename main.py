23dfd4f728a3900469d52ce62954582bc4a77cba
MaxAnii <ansarbaba2000@gmail.com>
2024-01-03 23:42:36 +0530

    change the endpoint to configure.

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ api_key_secret = os.getenv("API_KEY_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-api.update_status("testing the bot")
+client = tweepy.Client(consumer_key=api_key,
+                       consumer_secret=api_key_secret,
+                       access_token=access_token,
+                       access_token_secret=access_token_secret)
+client.create_tweet(text="tests")
