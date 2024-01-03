0e7a1c5858cb6be20406781447134199b8c25870
MaxAnii <ansarbaba2000@gmail.com>
2024-01-03 23:47:16 +0530

    Create the tweet.

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import tweepy
-from dotenv import load_dotenv
-load_dotenv()
-import os
+from config import client
 
-api_key = os.getenv("API_KEY")
-api_key_secret = os.getenv("API_KEY_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+client.create_tweet(text="second Test")
 
-client = tweepy.Client(consumer_key=api_key,
-                       consumer_secret=api_key_secret,
-                       access_token=access_token,
-                       access_token_secret=access_token_secret)
-client.create_tweet(text="tests")
+
+
+
