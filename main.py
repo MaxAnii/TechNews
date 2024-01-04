83576aebe430f26194e1368eb84b49314dbe5e7e
MaxAnii <ansarbaba2000@gmail.com>
2024-01-04 23:23:27 +0530

    Tweet news articles.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from config import client
+from config.tweepy_config import client
+from config.newApi_config import articles
 
-client.create_tweet(text="second Test")
 
+for article in articles:
+    source = article['source']
+    author = article['author']
+    title = article['title']
+    description = article['description']
+    url = article['url']
+    urlToImage = article['urlToImage']
+    publishedAt = article['publishedAt']
+    content = article['content']
+    tweet_text = f"Title: {title} \nSource: {source['name']}\n Author: {author} \n  URL: {url} \n Published At: {publishedAt}"
+    client.create_tweet(text=tweet_text)
 
 
 
