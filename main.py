e9c1b6efff768d01c14641bea235dfed11f9ab44
MaxAnii <ansarbaba2000@gmail.com>
2024-01-06 22:00:30 +0530

    Created server using FLASK and scheduled the tweet

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
+from flask import Flask
+import schedule
+import time
+from threading import Thread
 from config.tweepy_config import client
 from config.newApi_config import get_news
 
-data = get_news()
-articles = data['articles']
-print()
-for article in articles:
-    source = article['source']
-    author = article['author']
-    title = article['title']
-    url = article['url']
+app = Flask(__name__)
+
+def tweet():
+    try:
+        data = get_news()
+        articles = data['articles']
+        for article in articles:
+            source = article['source']
+            author = article['author']
+            title = article['title']
+            url = article['url']
+            tweet_text = f"{title}\nSource: {source['name']}\nURL: {url}\n@technology @ai"
+            if len(tweet_text) > 280:
+                print(f"Skipping tweet: exceeds 250 characters ({len(tweet_text)} characters)")
+            else:
+                print(tweet_text)
+                time.sleep(2)
+                client.create_tweet(text=tweet_text)
+    except:
+        print("Error in Tweeting")
+        
+def schedule_tweet():
+    # schedule.every().day.at("17:13").do(tweet)
+    schedule.every(3).hours.do(tweet)
+def run_flask():
+    app.run(debug=True)
     
-    publishedAt = article['publishedAt']
-    
-    tweet_text = f"{title}\nSource: {source['name']}\nAuthor: {author}\nRead full article: {url}\n@technology @ai"
-    if len(tweet_text) > 200:
-        pass
-    else:
-        client.create_tweet(text=tweet_text)
+if __name__ == '__main__':
+    schedule_tweet() 
+    flask_thread = Thread(target=run_flask)
+    flask_thread.start()
 
-
-
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
