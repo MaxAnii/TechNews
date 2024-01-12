bfd4dcf745e612328d80bd530cb46f58cb6d196b
MaxAnii <ansarbaba2000@gmail.com>
2024-01-12 20:38:17 +0530

    Made a search query more specific.

--- config/newApi_config.py
+++ config/newApi_config.py
@@ -4,10 +4,10 @@ import os
 load_dotenv()
 api_key = os.getenv("NEWSAPI_KEY")
 newsapi = NewsApiClient(api_key=api_key)
-
+search_query = os.getenv("TECH_KEY_WORDS")
 
 def get_news():
-    data = newsapi.get_everything(q='technology OR coding OR programing OR software OR ai OR pyhton OR java OR javaScript OR reactjs OR software developer',
+    data = newsapi.get_everything(q=search_query,
                                         page_size=5,
                                           language='en',
                                           sort_by='publishedAt',
